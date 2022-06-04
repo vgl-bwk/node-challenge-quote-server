a81c0c4f20fe4916158f8d0509e31b493cd9ed46
vgl-bwk <83692259+vgl-bwk@users.noreply.github.com>
2022-06-04 19:04:15 +0200

    Matches on quote or author

--- server.js
+++ server.js
@@ -27,6 +27,15 @@ app.get("/quotes/random", function (request, response) {
   response.send(pickFromArray(quotes));
 });
 
+app.get("/quotes/search", function (request, response) {
+  const reqTerm = request.query.term.toLowerCase();
+  const termIncl = quotes.filter(
+    (obj) =>
+      obj.quote.toLowerCase().includes(reqTerm) ||
+      obj.author.toLowerCase().includes(reqTerm)
+  );
+  response.send(termIncl);
+});
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
